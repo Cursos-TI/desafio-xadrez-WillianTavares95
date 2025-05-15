644073d45f98a567fdaf456875f1952a92c2311d
WillianTavares95 <williantavaressilva20@outlook.com.br>
2025-05-15 00:40:30 +0000

    atualizacao

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main () {
+    // bispo frente direta ou esquerda
+    //rainha qualquer lado
+    //torre vertical ou horizontal
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int i = 1;
+    int j = 1;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+   do
+   {
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("Bispo frente direita \n");
+    i++;
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+   } while (i <= 5);
+   
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+   for (int i = 0; i <= 7; i++)
+   printf("Rainha frente \n");
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+
+   while (j <= 5)
+   
+   {
+    printf("Torre esquerda \n");
+    j ++;
+   }
+   
+
+
+  for (int i = 0; i < 1; i++) 
+    {
+        for (int j = 0; j <= 1; j++)
+            printf("Cima \n");
+        
+    printf(" Esquerda \n");        
+       
+    }
+    
+    
 
     return 0;
 }
+
